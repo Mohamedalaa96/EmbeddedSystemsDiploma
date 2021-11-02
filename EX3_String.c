b403de2f23a36c6278a7cfba188b67001746acc8
Mohamedalaa96 <mohamedalaa_96@yahoo.com>
2021-11-02 18:24:25 +0200

    Reverse String

---
+++ EX3_String.c
@@ -0,0 +1,33 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 27, 2021
+ *      Author: Mohamed Alaa
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	/* to test two numbers for quick demonstration only */
+	char matrix[30];
+	printf("Enter the string: ");
+	fflush(stdout);
+	char temp;
+	int i=0;
+	while (scanf("%c",&temp) && temp != '\n' && temp != '\r'){
+		matrix[i++] = temp;
+	}
+	int k, j;
+	for (k =0, j = i-1; k < i/2  ; k++, j--){
+		temp = matrix[k];
+		matrix[k] = matrix[j];
+		matrix[j] = temp;
+	}
+	matrix[i] = '\0';
+	printf("Reversed string is: %s",matrix);
+
+
+	return 0;
+}
